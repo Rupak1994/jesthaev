99b12f5a028e6bdfa9db0475e2979f3836e86e16
rupak <roopakthapa5@gmail.com>
2024-07-07 16:51:31 +0545

    add more code in day1

--- python basic/day1.py
+++ python basic/day1.py
@@ -1,10 +1,12 @@
-print("this is 1st class")
-name="rupak"
-print("hello:",name)
-print("who are you")
-print("i am",input())
+# print("this is 1st class")
+# name="rupak"
+# print("hello:",name)
+# print("who are you")
+# print("i am",input())
+
+# to take input from user and display sum
 print("enter 1st number")
 a=int(input())
 print("enter 2nd number")
 b=int(input())
-print("sum:",a+b)
+print("the sum of",a,"and",b,"is",'  ', a+b)
